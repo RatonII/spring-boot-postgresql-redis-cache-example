b288103488a273fe2d56cf4f0e3ee6e83f5a1ddb
Marius Stanescu <marius.s@sliide.com>
2024-01-14 10:29:25 +0200

    Adding port variable

--- src/main/java/com/bezkoder/spring/redis/model/Tutorial.java
+++ src/main/java/com/bezkoder/spring/redis/model/Tutorial.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 
 @Entity
 @Table(name = "tutorials")
+@RedisHash("tutorials")
 public class Tutorial implements Serializable {
 
   private static final long serialVersionUID = 1L;
